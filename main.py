a23e1281adb74cc2a982da495f8ee64163bc1e82
Luis Cabrera Benito <parzibyte@gmail.com>
2021-03-02 15:07:13 -0600

    Cambios menores

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 """
     Script de demostración de uso. Recuerda que debes configurar la impresora y el conector.
-    Todo lo encuentras en: https://parzibyte.me/blog/2021/02/09/presentando-plugin-impresoras-termicas-version-2/
+    1. Descarga el conector:  https://github.com/parzibyte/ejemplos-plugin-impresoras-termicas-v2/releases/latest
+    2. Configura tu impresora y compártela
+    3. Ejecuta el conector (no abre ventanas)
+    4. Ejecuta este script.
+    Ante cualquier duda: https://parzibyte.me/blog/2021/02/09/presentando-plugin-impresoras-termicas-version-2/
 """
 from ConectorPlugin import Conector, AccionBarcodeJan13, AlineacionCentro
 
@@ -24,11 +28,16 @@ c.codigoDeBarras("7506129445966", AccionBarcodeJan13)
 c.qrComoImagen("Parzibyte")
 c.texto("Imagen de URL:\n")
 c.imagenDesdeUrl("https://github.com/parzibyte.png")
+c.texto("Imagen local:\n")
+# Recuerda que la imagen debe existir y debe ser legible para el plugin. Si no, comenta las líneas
+c.imagenLocal(
+    "C:\\Users\\Luis Cabrera Benito\\Desktop\\cliente_python_impresoras_termicas\\python-logo.png")
 c.feed(5)
 c.cortar()
 c.abrirCajon()
+print("Imprimiendo...")
 # Recuerda cambiar por el nombre de tu impresora
-respuesta = c.imprimirEn("POS58")
+respuesta = c.imprimirEn("ZJ-58")
 if respuesta == True:
     print("Impresión correcta")
 else:
