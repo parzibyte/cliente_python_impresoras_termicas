8955fd201b951ca889cc53b7773e36cf4e1b0f69
Luis Cabrera Benito <parzibyte@gmail.com>
2022-11-14 13:11:50 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,89 @@
 """
     Script de demostración de uso. Recuerda que debes configurar la impresora y el conector.
-    1. Descarga el conector:  https://github.com/parzibyte/ejemplos-plugin-impresoras-termicas-v2/releases/latest
-    2. Configura tu impresora y compártela
+    1. Descarga el conector:  https://github.com/parzibyte/plugin-impresora-termica-v3/releases/latest
+    2. Configura tu impresora y compártela: https://parzibyte.me/blog/2017/12/11/instalar-impresora-termica-generica/
     3. Ejecuta el conector (no abre ventanas)
     4. Ejecuta este script.
-    Ante cualquier duda: https://parzibyte.me/blog/2021/02/09/presentando-plugin-impresoras-termicas-version-2/
+    Ante cualquier duda: https://parzibyte.me/blog/2021/03/02/python-imprimir-impresora-termica/
 """
-from ConectorPlugin import Conector, AccionBarcodeJan13, AlineacionCentro
+import ConectorPython
 
-# Esto es para obtener las impresoras. No es obligatorio hacerlo siempre que se quiera imprimir
-impresoras = Conector.obtenerImpresoras()
-print(f"Las impresoras son: {impresoras}")
+impresoras = ConectorPython.ConectorV3.obtenerImpresoras()
+print("Las impresoras son:")
+print(impresoras)
 
-c = Conector()
-c.textoConAcentos("¡Me llamo María José!\n")
-c.establecerEnfatizado(1)
-c.texto("Año 2021\n")
-c.establecerEnfatizado(0)
-c.texto("Sin enfatizado\n")
-c.establecerTamanioFuente(2, 2)
-c.texto("Texto de 2, 2\n")
-c.establecerTamanioFuente(1, 1)
-c.establecerJustificacion(AlineacionCentro)
-c.texto("Texto centrado\n")
-c.texto("Código de barras:\n")
-c.codigoDeBarras("7506129445966", AccionBarcodeJan13)
-c.qrComoImagen("Parzibyte")
-c.texto("Imagen de URL:\n")
-c.imagenDesdeUrl("https://github.com/parzibyte.png")
-c.texto("Imagen local:\n")
-# Recuerda que la imagen debe existir y debe ser legible para el plugin. Si no, comenta las líneas
-c.imagenLocal(
-    "C:\\Users\\Luis Cabrera Benito\\Desktop\\cliente_python_impresoras_termicas\\python-logo.png")
-c.feed(5)
-c.cortar()
-c.abrirCajon()
-print("Imprimiendo...")
-# Recuerda cambiar por el nombre de tu impresora
-respuesta = c.imprimirEn("ZJ-58")
+nombreImpresora = "PT210"  # Nota: esta impresora debe existir y estar compartida como se indica en https://parzibyte.me/blog/2017/12/11/instalar-impresora-termica-generica/
+# Aquí va tu serial por si tienes uno:
+serial = ""
+
+amongUsComoCadena = """000001111000
+000010000100
+000100011110
+000100100001
+011100100001
+010100100001
+010100100001
+010100011110
+010100000010
+011100000010
+000100111010
+000100101010
+000111101110
+000000000000
+000000000000
+000000000000
+111010101110
+100010101000
+111010101110
+001010100010
+111011101110
+000000000000
+000000000000
+000000000000"""
+conector = ConectorPython.ConectorV3(serial=serial)
+conector.Iniciar()
+conector.DeshabilitarElModoDeCaracteresChinos()
+conector.EstablecerAlineacion(ConectorPython.ALINEACION_CENTRO)
+conector.DescargarImagenDeInternetEImprimir(
+    "http://assets.stickpng.com/thumbs/587e32259686194a55adab73.png", 0, 216)
+conector.Feed(1)
+conector.EscribirTexto("Parzibyte's blog\n")
+conector.EscribirTexto("Blog de un programador\n")
+conector.TextoSegunPaginaDeCodigos(2, "cp850", "Teléfono: 123456798\n")
+conector.EscribirTexto("Fecha y hora: 29/9/2022")
+conector.Feed(1)
+conector.EstablecerAlineacion(ConectorPython.ALINEACION_IZQUIERDA)
+conector.EscribirTexto("____________________\n")
+conector.TextoSegunPaginaDeCodigos(
+    2, "cp850", "Venta de plugin para impresoras versión 3\n")
+conector.EstablecerAlineacion(ConectorPython.ALINEACION_DERECHA)
+conector.EscribirTexto("$25\n")
+conector.EscribirTexto("____________________\n")
+conector.EscribirTexto("TOTAL: $25\n")
+conector.EscribirTexto("____________________\n")
+conector.EstablecerAlineacion(ConectorPython.ALINEACION_CENTRO)
+conector.HabilitarCaracteresPersonalizados()
+conector.DefinirCaracterPersonalizado("$", amongUsComoCadena)
+conector.EscribirTexto(
+    "En lugar del simbolo de pesos debe aparecer un among us\n")
+conector.EscribirTexto("TOTAL: $25\n")
+conector.EstablecerEnfatizado(True)
+conector.EstablecerTamañoFuente(1, 1)
+conector.TextoSegunPaginaDeCodigos(2, "cp850", "¡Gracias por su compra!\n")
+conector.Feed(1)
+conector.ImprimirCodigoQr("https://parzibyte.me/blog", 160,
+                          ConectorPython.RECUPERACION_QR_MEJOR, ConectorPython.TAMAÑO_IMAGEN_NORMAL)
+conector.Feed(1)
+conector.ImprimirCodigoDeBarrasCode128(
+    "parzibyte.me", 80, 192, ConectorPython.TAMAÑO_IMAGEN_NORMAL)
+conector.Feed(1)
+conector.EstablecerTamañoFuente(1, 1)
+conector.EscribirTexto("parzibyte.me\n")
+conector.Feed(3)
+conector.Corte(1)
+conector.Pulso(48, 60, 120)
+respuesta = conector.imprimirEn(nombreImpresora)
 if respuesta == True:
-    print("Impresión correcta")
+    print("Impreso correctamente")
 else:
-    print(f"Error. El mensaje es: {respuesta}")
+    print("Error: " + respuesta)
